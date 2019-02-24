88fb76f2d81d3f658dbb5a340615d4e0443d0a0b
zohaib <zohaibansari100@gmail.com>
2019-02-24 16:54:11 +0530

    tabs function added

--- app/src/main/java/tk/zedlabs/artmedia/fragments/ProfileTab.java
+++ app/src/main/java/tk/zedlabs/artmedia/fragments/ProfileTab.java
@@ -1,31 +1,90 @@
 package tk.zedlabs.artmedia.fragments;
 
-
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import tk.zedlabs.artmedia.R;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ProfileTab extends Fragment {
 
+private EditText nameEditText, hobbiesEditText, bioEditText;
+private Button updateButton;
 
-    public ProfileTab() {
-        // Required empty public constructor
-    }
-
+    public ProfileTab(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        nameEditText = view.findViewById(R.id.nameEditText);
+        hobbiesEditText = view.findViewById(R.id.hobbiesEditText);
+        bioEditText = view.findViewById(R.id.bioEditText);
+        updateButton = view.findViewById(R.id.updateButton);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        String nickname = parseUser.get("nickname")+"";
+        String hobbies = parseUser.get("hobbies")+"";
+        String bio = parseUser.get("bio")+"";
+
+        if(parseUser.get("nickname") == null){
+            nameEditText.setText("");
+        }
+         else {
+            nameEditText.setText(nickname);
+              }
+
+        if(parseUser.get("hobbies") == null){
+            hobbiesEditText.setText("");
+        }
+          else {
+            hobbiesEditText.setText(hobbies);
+               }
+
+        if(parseUser.get("bio") == null){
+            bioEditText.setText("");
+        }
+          else {
+            bioEditText.setText(bio);
+               }
+
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseUser.put("nickname",nameEditText.getText()+"");
+                parseUser.put("hobbies",hobbiesEditText.getText()+"");
+                parseUser.put("bio",bioEditText.getText()+"");
+
+                final ProgressDialog progressDialog = new ProgressDialog(getContext());
+                progressDialog.setMessage("Updating User info");
+                progressDialog.show();
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            progressDialog.hide();
+                            Toast.makeText(getContext(),"profile updated!",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        return view;
     }
 
 }
--- app/src/main/java/tk/zedlabs/artmedia/fragments/UsersTab.java
+++ app/src/main/java/tk/zedlabs/artmedia/fragments/UsersTab.java
@@ -6,26 +6,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import tk.zedlabs.artmedia.R;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class UsersTab extends Fragment {
 
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
 
-    public UsersTab() {
-        // Required empty public constructor
-    }
+    public UsersTab(){}
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+        listView = view.findViewById(R.id.userList);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1, arrayList);
 
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("nickname",ParseUser.getCurrentUser());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> userList, ParseException e) {
+                if(e == null){
+                    if(userList.size()>0){
+
+                        for(ParseUser user : userList){
+                            arrayList.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);             //adapter is added here because arrayList was not populated earlier
+                    }
+                }
+            }
+        });
+        return view;
+    }
 }
